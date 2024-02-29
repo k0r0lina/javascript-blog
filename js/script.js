0294c475d7b562002a551e627d8a8b97087463f8
Karolina Rząsa <karolina.rzasa2002@gmail.com>
2024-02-29 14:32:56 +0100

    Add clickedElement

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log('Link was clicked!');
-  console.log(event);
+    const clickedElement = this;
+    console.log('Link was clicked!');
+
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -10,9 +12,13 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
-  const activeArticles = document.querySelectorAll('.posts .post.active');
+  /* [DONE] remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.posts .post.active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
