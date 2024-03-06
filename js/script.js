38a659967ee1c1d9c2cfe6ed9c295f4b99b306e7
Karolina Rząsa <karolina.rzasa2002@gmail.com>
2024-03-06 15:15:46 +0100

    Add ESLint

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 'use strict';
 
 function titleClickHandler(event){
-    event.preventDefault();
-    const clickedElement = this;
-    console.log('Link was clicked!');
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!');
 
   /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -24,7 +24,7 @@ function titleClickHandler(event){
   }
 
   /* get 'href' attribute from the clicked link */
-  const articleSelector = clickedElement.getAttribute("href");
+  const articleSelector = clickedElement.getAttribute('href');
   console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
